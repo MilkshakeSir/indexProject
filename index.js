a83a117bddffb4c56fff87b285a04c1de54701eb
MilkshakeSir <93629950+MilkdevNew@users.noreply.github.com>
2023-05-17 13:12:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var linkInput = document.getElementById("linkInput");
-    var linkList = document.getElementById("linkList");
-    var videoContainer = document.getElementById("videoContainer");
-  
-    function getYoutubeId(url) {
-      var youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=|v\/|ytscreeningroom\?v=|e\/|u\/\w+\/|embed\/|v=|ytscreeningroom\?v=|e\/|u\/\w+\/))?(?:[\w-]{11})(?:[\?&][\w\?=]*)?$/;
-      var match = url.match(youtubeRegex);
-  
-      if (match && match.length > 0) {
-        return match[0].substring(match[0].length - 11);
-      } else {
-        return null;
-      }
+  var linkInput = document.getElementById("linkInput");
+  var linkList = document.getElementById("linkList");
+  var videoContainer = document.getElementById("videoContainer");
+
+  function getYoutubeId(url) {
+    var youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=|v\/|ytscreeningroom\?v=|e\/|u\/\w+\/|embed\/|v=|ytscreeningroom\?v=|e\/|u\/\w+\/))?(?:[\w-]{11})(?:[\?&][\w\?=]*)?$/;
+    var match = url.match(youtubeRegex);
+
+    if (match && match.length > 0) {
+      return match[0].substring(match[0].length - 11);
+    } else {
+      return null;
     }
-  
-    function handleLink() {
-      var linkUrl = linkInput.value;
-  
-      if (linkUrl) {
-        var videoId = getYoutubeId(linkUrl);
-        if (videoId) {
-          var embedCode = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
-  
-          var listItem = document.createElement("li");
-          listItem.innerHTML = embedCode;
-          listItem.addEventListener('click', function() {
-            videoContainer.innerHTML = this.innerHTML;
-          });
-  
-          linkList.appendChild(listItem);
-        }
-      }
+  }
+
+  function embedVideo(videoId) {
+    videoContainer.innerHTML = ''; // Clear any existing video
+
+    if (videoId) {
+      var embedCode = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
+      videoContainer.innerHTML = embedCode;
     }
-  
-    document.getElementById("addButton").addEventListener('click', handleLink);
-  });
-  
+  }
+
+  function addLink() {
+  var linkUrl = linkInput.value;
+  var videoId = getYoutubeId(linkUrl);
+
+  if (videoId) {
+    var listItem = document.createElement('li');
+    var linkItem = document.createElement('a');
+    linkItem.href = '#';
+    linkItem.innerHTML = linkUrl;
+    linkItem.addEventListener('click', function() {
+      embedVideo(videoId);
+    });
+
+    listItem.appendChild(linkItem);
+    linkList.insertBefore(listItem, linkList.firstChild); // Insert the new item at the beginning of the list
+
+    embedVideo(videoId); // Embed the video immediately
+
+    linkInput.value = ''; // Clear the input field
+  }
+}
+
+
+  document.getElementById("addButton").addEventListener('click', addLink);
+});
