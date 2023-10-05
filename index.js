3ddc3bdf67861c363c151b348d85763fb5804a24
MilkshakeSir <93629950+MilkshakeSir@users.noreply.github.com>
2023-10-05 18:22:40 +0000

    Update and added UrlSearchParams

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var linkInput = document.getElementById("linkInput");
-  var linkList = document.getElementById("linkList");
-  var videoContainer = document.getElementById("videoContainer");
+// Init function
+// Read url for ?video
 
-  function getYoutubeId(url) {
-    var youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=|v\/|ytscreeningroom\?v=|e\/|u\/\w+\/|embed\/|v=|ytscreeningroom\?v=|e\/|u\/\w+\/))?(?:[\w-]{11})(?:[\?&][\w\?=]*)?$/;
-    var match = url.match(youtubeRegex);
+const queryUrl = window.location.search
+const urlParams = new URLSearchParams(queryUrl) 
 
-    if (match && match.length > 0) {
-      return match[0].substring(match[0].length - 11);
-    } else {
-      return null;
-    }
+var linkInput = document.getElementById("linkInput");
+var videoContainer = document.getElementById("videoContainer");
+
+const videoUrlFromLink = urlParams.get("video")
+
+function getYoutubeId(url) {
+  var youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=|v\/|ytscreeningroom\?v=|e\/|u\/\w+\/|embed\/|v=|ytscreeningroom\?v=|e\/|u\/\w+\/))?(?:[\w-]{11})(?:[\?&][\w\?=]*)?$/;
+  var match = url.match(youtubeRegex);
+
+  if (match && match.length > 0) {
+    return match[0].substring(match[0].length - 11);
+  } else {
+    return null;
   }
+}
 
-  function embedVideo(videoId) {
-    videoContainer.innerHTML = ''; // Clear any existing video
 
-    if (videoId) {
-      var embedCode = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
-      videoContainer.innerHTML = embedCode;
-    }
+function embedVideo(videoId) {
+  videoContainer.innerHTML = ''; // Clear any existing video
+
+  if (videoId) {
+    var embedCode = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
+    videoContainer.innerHTML = embedCode;
   }
+}
+
+if (videoUrlFromLink) {
+  const videoId = getYoutubeId(videoUrlFromLink)
+  embedVideo(videoId)
+}
+
+
+document.addEventListener('DOMContentLoaded', function() { // Start function
+
 
   function addLink() {
   var linkUrl = linkInput.value;
   var videoId = getYoutubeId(linkUrl);
 
   if (videoId) {
-    var listItem = document.createElement('li');
-    var linkItem = document.createElement('a');
-    linkItem.href = '#';
-    linkItem.innerHTML = linkUrl;
-    linkItem.addEventListener('click', function() {
-      embedVideo(videoId);
-    });
-
-    listItem.appendChild(linkItem);
-    linkList.insertBefore(listItem, linkList.firstChild); // Insert the new item at the beginning of the list
 
     embedVideo(videoId); // Embed the video immediately
 
